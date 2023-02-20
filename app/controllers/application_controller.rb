23f811cf9579dcd7795752a4e2c7ee0a367854d6
Simon <simonlucas1996@gmail.com>
2023-02-20 16:50:27 -0300

    Paginate and search for records

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid do |invalid|
     render json: {errors: invalid.record.errors}, status: 422
   end
+
+
+  def per_page
+    params[:per_page] || 10
+  end
+
+  def page
+    params[:page] || 1
+  end
 end
--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -1,3 +1,15 @@
 class ActivityType < ApplicationRecord
   validates_presence_of :name, :description
+
+  class << self
+    def search(params)
+      scope = where({})
+      scope = global_search(params[:search]) if params[:search]
+      scope
+    end
+
+    def global_search(text)
+      where("name LIKE :search OR description LIKE :search", search: "%#{text}%")
+    end
+  end
 end
