a68a6534292dd187e38033268e862384f0cc60c1
Magali Cabrera <magymonse24@gmail.com>
2023-05-20 16:41:26 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :username
-  validates_presence_of :email, uniqueness: { case_sensitive: false }
+  validates_presence_of :username, :email, uniqueness: { case_sensitive: false }
 
   class << self
     def search(params)
